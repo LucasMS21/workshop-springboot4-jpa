75323ae9a7f388d4a406d96432b5115084dc7593
LucasMS21 <lucasedu.6a@gmail.com>
2026-07-22 17:09:55 -0300

    Service layer, component registration

--- src/main/java/com/lucasm/coursejava/repositories/UserRepository.java
+++ src/main/java/com/lucasm/coursejava/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.lucasm.coursejava.repositories;
 
 import com.lucasm.coursejava.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
