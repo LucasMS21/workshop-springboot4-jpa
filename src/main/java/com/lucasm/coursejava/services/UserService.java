94f68178934c857bc339574eb216c4eca53622f1
LucasMS21 <lucasedu.6a@gmail.com>
2026-08-07 13:55:14 -0300

    Exception handling - update

--- src/main/java/com/lucasm/coursejava/services/UserService.java
+++ src/main/java/com/lucasm/coursejava/services/UserService.java
@@ -4,6 +4,7 @@ import com.lucasm.coursejava.entities.User;
 import com.lucasm.coursejava.repositories.UserRepository;
 import com.lucasm.coursejava.services.exceptions.DatabaseException;
 import com.lucasm.coursejava.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +46,15 @@ public class UserService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
