ab7ea869823c2289de79c5ee88cd9983055c698f
LucasMS21 <lucasedu.6a@gmail.com>
2026-08-07 13:49:11 -0300

    Exception handling - delete

--- src/main/java/com/lucasm/coursejava/services/UserService.java
+++ src/main/java/com/lucasm/coursejava/services/UserService.java
@@ -2,8 +2,11 @@ package com.lucasm.coursejava.services;
 
 import com.lucasm.coursejava.entities.User;
 import com.lucasm.coursejava.repositories.UserRepository;
+import com.lucasm.coursejava.services.exceptions.DatabaseException;
 import com.lucasm.coursejava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -30,7 +33,15 @@ public class UserService {
     }
 
     public void delete(Long id){
+        try {
         repository.deleteById(id);
+        }
+        catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
